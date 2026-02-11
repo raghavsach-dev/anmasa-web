0923fa5fe0846dedfeb5cd88a0f6d7fd2784166d
raghav sachdev <raghav@iTuples-MacBook-Pro.local>
2026-02-11 16:20:59 +0530

    Luluuuuuu

--- src/components/product/Quantity.tsx
+++ src/components/product/Quantity.tsx
@@ -1,13 +1,26 @@
+"use client"
+import { useState } from "react";
+
 export default function Quantity({ outOfStock }: { outOfStock: boolean }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantity(parseInt(e.target.value));
+  };
+
   return (
-    <div className="flex flex-row gap-2">
-      <button className="px-3 py-2 border rounded-2xl text-sm">
+    <>
+    <p className="text-sm text-gray-500">Quantity</p>
+    <div className="flex flex-row gap-2 items-center border border-anmasa-accent rounded-lg p-2 w-fit px-4">
+        
+      <button disabled={quantity <= 1} onClick={() => setQuantity(quantity - 1)}>
         -
       </button>
-      <input type="number" className="px-3 py-2 border rounded-2xl text-sm"/>
-      <button className="px-3 py-2 border rounded-2xl text-sm">
+      <input type="number" value={quantity} onChange={handleQuantityChange} className="w-10 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" />
+      <button onClick={() => setQuantity(quantity + 1)}>
         +
       </button>
     </div>
+    </>
   )
 }
