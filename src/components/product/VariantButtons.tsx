cb7692791973552d86aef651b1b6276853b92085
Raghav Sachdev <iraghavsachdev@gmail.com>
2026-02-20 14:13:16 +0530

    jfb

--- src/components/product/VariantButtons.tsx
+++ src/components/product/VariantButtons.tsx
@@ -26,7 +26,7 @@ export default function VariantButtons({
           type="button"
         >
           {variant.vn}
-          <span className="absolute -top-1 -right-1 text-xs text-white bg-anmasa-accent text-anmasa-text rounded-full px-2 py-1">
+          <span className="absolute -top-2 -right-2 text-xs text-white bg-anmasa-accent text-anmasa-text rounded-full px-2 py-1 z-10">
             1
           </span>
         </button>
