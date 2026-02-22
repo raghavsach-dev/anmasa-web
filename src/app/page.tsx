8b35ca1b4f82720dfb3f6dccbcf525571ef0ddba
Dhruvi Mittal <iraghavsachdev@gmail.com>
2026-02-22 23:32:02 +0530

    category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import CategoryRow from "@/components/category/CategoryRow";
+// import CategoryRow from "@/components/category/CategoryRow";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ProductRow from "@/components/products/ProductRow";
 import Image from "next/image";
 
 export default async function Home() {
-
   return (
     <div className="bg-anmasa-bg w-[100vw]">
       <>
         <Navbar />
-        <ProductRow categoryCode = "ATTA" />
+        <ProductRow categoryCode="ATTA" />
         <Image
           src="/promo.png"
           alt="promo"
@@ -18,7 +17,7 @@ export default async function Home() {
           height={1400}
           className="object-cover"
         />
-        <CategoryRow categoryCode="ATTA" />
+        {/* <CategoryRow categoryCode="ATTA" /> */}
       </>
     </div>
   );
