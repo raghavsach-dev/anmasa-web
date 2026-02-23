48582ef1272554127f9fd73e0384ca107ef4cf04
Dhruvi Mittal <iraghavsachdev@gmail.com>
2026-02-23 14:55:45 +0530

    category

--- src/components/category/CategoryList.tsx
+++ src/components/category/CategoryList.tsx
@@ -1,6 +1,5 @@
 import { Categories } from "@/app/types/categories";
 import { getAllCategories } from "@/app/services/category-service";
-import CategoryCard from "./CategoryCard";
 import Image from "next/image";
 
 export default async function CategoryList() {
@@ -11,18 +10,21 @@ export default async function CategoryList() {
   return (
     <div className="grid grid-cols-3 gap-6">
       {categories.map((category) => (
-        <div className="shrink-0  bg-transparent rounded w-[250px] h-[440px] flex flex-col items-center justify-center ">
-          <div className="relative">
+        <div
+          key={category.code}
+          className="shrink-0  bg-transparent rounded w-[250px] h-[440px] flex flex-col items-center justify-center "
+        >
+          <div className="relative w-[250px] h-[250px] mx-auto">
             <Image
               src={category.image_url || "/oils4.webp"}
               alt={category.name}
-              width={250}
-              height={250}
-              className="object-cover mx-auto rounded rounded-lg"
+              width={100}
+              height={100}
+              className="object-cover rounded rounded-lg"
             />
           </div>
-          <div className="mt-1 text-md text-gray-500">
-            <p className="m-0 text-center">{category.name}</p>
+          <div className="mt-1 text-md text-gray-500 text-center">
+            <p className="m-0">{category.name}</p>
           </div>
         </div>
       ))}
